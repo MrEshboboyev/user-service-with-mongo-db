4d2fefba2f4f6be4c533a57c67a42fa6f574e8cc
MrEshbobotev <mreshboboyev@gmail.com>
2025-03-04 17:10:03 +0500

    Adding CRUD of Mongo Db about USer

---
+++ UserServiceApi/Extensions/RepositoryExtension.cs
@@ -0,0 +1,13 @@
+﻿using UserServiceRepository.Interface;
+using UserServiceRepository;
+
+namespace UserServiceApi.Extensions;
+
+public static class RepositoryExtension
+{
+    public static IServiceCollection AddRepository(this IServiceCollection services)
+    {
+        services.AddScoped<IUserRepository, UserRepository>();
+        return services;
+    }
+}
